83a4a57d5ead2b906a29de62502b3d88823d5a48
yamkela-macwili <84202051+yamkela-macwili@users.noreply.github.com>
2021-05-17 00:20:48 +0200

    Add files via upload

---
+++ ProgrammingIsFunApp.java
@@ -0,0 +1,10 @@
+//ProgrammingIsFunApp.java
+import javax.swing.JOptionPane;
+public class ProgrammingIsFunApp {
+	
+	public static void main(String[] args)
+	{
+		JOptionPane.showMessageDialog(null, "Programming is Fun!");
+		System.exit(0);
+	}
+}
